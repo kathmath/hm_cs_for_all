89c844bbb3716f399a8f3b210afca0adad0d125d
Kathleen Matthews <kathmath@gmail.com>
2015-08-18 22:07:36 -0700

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# hm_cs_for_all
+Exercises from Harvey Mudd's CS For All course http://www.cs.hmc.edu/csforall/index.html
---
+++ chapter2.py
@@ -0,0 +1,13 @@
+def collatz( n ):
+	'''Takes a single number as an argument and applies collatz to it'''
+	if n % 2 == 0:
+		return n/2
+	else:
+		return 3*n + 1
+
+
+
+		Hey guys,  if you want to get me something for my birthday i would like a 4 pack of golf lessons for$200 from proshop golf,  661-496-8570.  Thx.  Dad.  Took a lesson today.
+
+
+		john
